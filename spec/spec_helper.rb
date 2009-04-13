206ea35e2bd2c354a1fed075e5cb8bf63f656126
Harold A. Gimenez <harold.gimenez@gmail.com>
2009-04-13 11:39:53 -0400

    Removed commented lines, cleaning up code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,9 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'peegee'
 
 require 'spec/peegee_helper'
-#ActiveRecord::Base.logger = Logger.new(StringIO.new)
-#
 require 'factory_girl'
 
 Spec::Runner.configure do |config|
   Kernel.const_set :RAILS_ROOT, "#{Dir.pwd}/tmp" unless defined?(RAILS_ROOT)
-  #PeegeeHelper.new
   require 'spec/fixtures/activerecord_models'
 end
