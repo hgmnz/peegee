19588cad34bb7a3ad3a8dc378b729540f6fbd0c0
Harold A. Gimenez <harold.gimenez@gmail.com>
2009-04-13 11:39:20 -0400

    Formatted comments for RDoc

--- lib/peegee/configuration.rb
+++ lib/peegee/configuration.rb
@@ -4,13 +4,13 @@ module Peegee
   class Configuration
     include Singleton
 
-    # the mappings between tables and indexes, used 
-    # for executing the postgres CLUSTER command.
+    # The mappings between tables and indexes, used 
+    # for executing Peegee's implementation of the Postgres CLUSTER command.
     # The hash maps tables to indexes. You may pass
     # either a 'string' or a :symbol.
     # For example: 
-    # cluster_indexes = {:posts => :ix_posts_on_active_and_created_at,
-    #                    'users' => 'ix_user_on_active_and_person_id'}
+    # <tt>cluster_indexes = {:posts => :ix_posts_on_active_and_created_at,
+    #                    'users' => 'ix_user_on_active_and_person_id'}</tt>
     @cluster_indexes = {}
     attr_accessor :cluster_indexes
 
