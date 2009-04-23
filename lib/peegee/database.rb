62524f7d9b0e97ef7537d4bbd5b92a5462ddb238
Harold A. Gimenez <hgimenez@hgnux.(none)>
2009-04-23 19:02:27 -0400

    Removed osbolete (and commented) method

--- lib/peegee/database.rb
+++ lib/peegee/database.rb
@@ -1,20 +1,15 @@
 module Peegee
   class Database
 
-    #def self.object_exists?(object_name)
-      #sql = <<-END_SQL
-        #select * from pg_class where relname = '#{object_name}' limit 1
-      #END_SQL
-      #ActiveRecord::Base.connection.execute(sql).entries.size > 0
-    #end
-
-    def self.register_table(table)
-      if (@tables ||= {}).has_key?(table.table_name.to_sym)
-        return @tables[table.table_name.to_sym]
-      else
-        @tables[table.table_name.to_sym] = table
-        return table
+    protected 
+      def self.register_table(table)
+        if (@tables ||= {}).has_key?(table.table_name.to_sym)
+          return @tables[table.table_name.to_sym]
+        else
+          @tables[table.table_name.to_sym] = table
+          return table
+        end
       end
-    end
+
   end
 end
